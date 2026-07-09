c953700e40637b182a2a4e018c84ba3537ac61dc
artsukach94-design <art.sukach94@gmail.com>
2026-07-09 17:26:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
+export const supabase = createClient(
+  'https://elrmeekxaxcyxdyzqtzg.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVscm1lZWt4YXhjeXhkeXpxdHpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MDA4MTIsImV4cCI6MjA5OTE3NjgxMn0.XfLB_ZPMbnnlK-TJX05vVipk6XxZPZlq95sbIAwDwFs'
+)
