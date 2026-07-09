8e0cd61ea9f4bb0820ef7245a61e3f471452e4bf
artsukach94-design <art.sukach94@gmail.com>
2026-07-09 17:31:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ export const supabase = createClient(
   'https://elrmeekxaxcyxdyzqtzg.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVscm1lZWt4YXhjeXhkeXpxdHpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MDA4MTIsImV4cCI6MjA5OTE3NjgxMn0.XfLB_ZPMbnnlK-TJX05vVipk6XxZPZlq95sbIAwDwFs'
 )
+export async function login(email, password) {
+  const { data, error } = await supabase.auth.signInWithPassword({ email, password })
+  if (error) throw error
+  return data
+}
